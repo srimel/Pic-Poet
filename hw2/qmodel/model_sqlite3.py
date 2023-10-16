8cf6c631f8da16302bcabd9381689cd7df2e2979
Stu <78074492+srimel@users.noreply.github.com>
2023-10-15 17:40:11 -0700

    update model for select, insert ops

--- hw2/qmodel/model_sqlite3.py
+++ hw2/qmodel/model_sqlite3.py
@@ -18,7 +18,26 @@ class model(Model):
         cursor.close()
 
     def select(self):
-        pass
+        connection = sqlite3.connect(DB_FILE)
+        cursor = connection.cursor()
+        cursor.execute("select * from quotes")
+        return cursor.fetchall()
 
-    def insert(self):
-        pass
+    def insert(self, quote, author, date, type, source, rating):
+        params = {
+            "quote": quote,
+            "author": author,
+            "date": date,
+            "type": type,
+            "source": source,
+            "rating": rating,
+        }
+        connection = sqlite3.connect(DB_FILE)
+        cursor = connection.cursor()
+        cursor.execute(
+            "insert into quotes values (quote, author, date, type, source, rating) values (:quote, :author, :date, :type, :source, :rating)",
+            params,
+        )
+        connection.commit()
+        cursor.close()
+        return True
