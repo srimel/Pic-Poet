66deaaed22affd39508a34e71d5fceb5736a90f4
Stuart Rimel <stuart.rimel@gmail.com>
2023-11-08 11:33:06 -0800

    add docstring documentation to datastore model

--- hw4/qmodel/model_datastore.py
+++ hw4/qmodel/model_datastore.py
@@ -2,6 +2,14 @@ from .Model import Model
 from google.cloud import datastore
 
 def from_datastore(entity):
+    """Helper function to transform Datastore results into the format expected
+
+    Args:
+        entity: entity from Datastore
+
+    Returns:
+        List: returns a list of the entity's values
+    """
     if not entity:
         return None
     if isinstance(entity, list):
@@ -13,11 +21,26 @@ class model(Model):
         self.client = datastore.Client('cloud-rimel-srimel')
 
     def select(self):
+        """Selects all quotes from the datastore of kind 'Quote'
+
+        Returns:
+            List: returns a list of all quotes in the datastore
+        """
         query = self.client.query(kind = 'Quote')
         entities = list(map(from_datastore, query.fetch()))
         return entities
     
     def insert(self, quote, author, date, type, source, rating):
+        """Inserts a quote into the datastore of kind 'Quote'
+
+        Args:
+            quote (string): quote to be inserted
+            author (string): author of the quote
+            date (date): date of the quote
+            type (string): type of the quote
+            source (string): source of the quote
+            rating (float): rating of the quote
+        """
         key = self.client.key('Quote')
         qt = datastore.Entity(key)
         qt.update( {
