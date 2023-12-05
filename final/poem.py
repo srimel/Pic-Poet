e2fe8e3a02222303b3d00c67aeb7cdbead3a05fb
Stu <78074492+srimel@users.noreply.github.com>
2023-12-04 21:20:44 -0800

    refined poem return as json, added title field, formatted html to use <br> for proper spacing

--- final/poem.py
+++ final/poem.py
@@ -1,3 +1,4 @@
+import json
 from flask import redirect, request, url_for, render_template, flash
 from flask.views import MethodView
 from openai import OpenAI
@@ -6,9 +7,20 @@ from openai import OpenAI
 class Poem(MethodView):
     def get(self):
         # TODO: fix format of poem string for nice output
-        poem = self.createPoem(request.args.get("labels"), request.args.get("objects"))
         image_path = request.args.get("image_path")
-        return render_template("poem.html", image_path=image_path, poem=poem)
+        # returns as json object with keys: title, poem
+        # if there was an error it will return with key: error
+        poem = self.createPoem(request.args.get("labels"), request.args.get("objects"))
+        if "error" in poem:
+            title = "Error"
+            poem = poem["error"]
+        else:
+            title = poem["title"]
+            poem = poem["poem"]
+
+        return render_template(
+            "poem.html", image_path=image_path, title=title, poem=poem
+        )
 
     def createPoem(self, labels, objects):
         client = OpenAI()
@@ -19,13 +31,17 @@ class Poem(MethodView):
                 {"role": "user", "content": self.createUserPrompt(labels, objects)},
             ],
         )
-        print(completion.choices[0].message)
-        return completion.choices[0].message
+        poem = completion.choices[0].message.content
+        try:
+            poem_json = json.loads(poem)
+            return poem_json
+        except ValueError as e:
+            return {"error": "Error parsing poem json"}
 
     def createUserPrompt(self, labels, objects):
-        prompt = f"Create a poem about {labels} and {objects}. Only ouput the poem in json format with key 'poem'."
+        prompt = f"Create a poem about {labels} and {objects}."
         return prompt
 
     def createSystemRole(self):
-        role_prompt = "You are a master artist of poetry who is skilled in creating poems of all styles."
+        role_prompt = 'You are a master artist of poetry who is skilled in creating poems of all styles. When you answer the user\'s prompt, please output in json in the following format: {"title": <title>, "poem": <poem>}'
         return role_prompt
