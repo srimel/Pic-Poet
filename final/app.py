2f586c4bcb12b783bf30075e0dcd3d82e132d218
Stu <78074492+srimel@users.noreply.github.com>
2023-12-04 19:48:39 -0800

    fix flash messages for user error messages

--- final/app.py
+++ final/app.py
@@ -1,9 +1,11 @@
+import os
 import flask
 from flask.views import MethodView
 from index import Index
 from search import Search
 
 app = flask.Flask(__name__)
+app.secret_key = os.environ["APP_SECRET_KEY"]
 
 app.add_url_rule("/", view_func=Index.as_view("index"), methods=["GET"])
 app.add_url_rule("/search", view_func=Search.as_view("search"), methods=["POST"])
