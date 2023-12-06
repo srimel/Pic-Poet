6bde9e1b7100f0fff7375cdabd85a4b147c32ea5
Stu <78074492+srimel@users.noreply.github.com>
2023-12-05 23:21:47 -0800

    add download to PDF feature

--- final/app.py
+++ final/app.py
@@ -4,6 +4,7 @@ from flask.views import MethodView
 from index import Index
 from search import Search
 from poem import Poem
+from download import Download
 
 app = flask.Flask(__name__)
 app.secret_key = os.environ["APP_SECRET_KEY"]
@@ -11,6 +12,7 @@ app.secret_key = os.environ["APP_SECRET_KEY"]
 app.add_url_rule("/", view_func=Index.as_view("index"), methods=["GET"])
 app.add_url_rule("/search", view_func=Search.as_view("search"), methods=["POST"])
 app.add_url_rule("/poem", view_func=Poem.as_view("poem"), methods=["GET"])
+app.add_url_rule("/download", view_func=Download.as_view("download"), methods=["GET"])
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
