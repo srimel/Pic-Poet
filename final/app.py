5038e217d0f2a672384bdc685c2ea8693a3420e9
Stu <78074492+srimel@users.noreply.github.com>
2023-12-06 18:10:46 -0800

    create about page

--- final/app.py
+++ final/app.py
@@ -5,6 +5,7 @@ from index import Index
 from search import Search
 from poem import Poem
 from download import Download
+from about import About
 
 app = flask.Flask(__name__)
 app.secret_key = os.environ["APP_SECRET_KEY"]
@@ -13,6 +14,7 @@ app.add_url_rule("/", view_func=Index.as_view("index"), methods=["GET"])
 app.add_url_rule("/search", view_func=Search.as_view("search"), methods=["POST"])
 app.add_url_rule("/poem", view_func=Poem.as_view("poem"), methods=["GET"])
 app.add_url_rule("/download", view_func=Download.as_view("download"), methods=["GET"])
+app.add_url_rule("/about", view_func=About.as_view("about"), methods=["GET"])
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
